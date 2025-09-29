d20687f1e8b54073db4e557f466bfb89cbd81275
Anton <antoncodes@outlook.com>
2025-09-29 23:15:49 +0300

    e2e: index route - pass

--- app/routes/_m._index.tsx
+++ app/routes/_m._index.tsx
@@ -1,5 +1,11 @@
-import { redirect } from "react-router";
+import { redirect } from 'react-router';
+import type { Route } from './+types/_m._index';
+import { getAccessTokenFromRequest } from '~/sessions/auth';
 
-export async function loader() {
-  return redirect('/login');
+export async function loader({ request }: Route.LoaderArgs) {
+  const accessToken = await getAccessTokenFromRequest(request);
+  if (!accessToken) {
+    return redirect('/login');
+  }
+  return redirect('/restricted');
 }
