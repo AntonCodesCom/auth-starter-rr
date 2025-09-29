0ee46a717a5c0e73c3ab459f639dea82204572bc
Anton <antoncodes@outlook.com>
2025-09-29 21:36:48 +0300

    index route e2e init

---
+++ tests/index.spec.ts
@@ -0,0 +1,16 @@
+import test from "@playwright/test";
+
+/**
+ * e2e
+ */
+test.skip('authenticated', async ({ page }) => {})
+
+/**
+ * e2e
+ */
+test.skip('no access token', async ({ page }) => {})
+
+/**
+ * e2e
+ */
+test.skip('invalid access token', async ({ page }) => {})
